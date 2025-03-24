5b2d393ac65b14f264e2fe4bd089ccd20ea7885f
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-03-24 11:25:54 +0700

    fix some sh

---
+++ Owls/setup.bat
@@ -0,0 +1,34 @@
+@echo off
+title Owls Bot by @MeoMunDep
+color 0A
+
+cd %~dp0
+
+echo Checking configuration files...
+
+(for %%F in (datas.txt proxies.txt) do (
+    if not exist %%F (
+        type nul > %%F
+        echo Created %%F
+    )
+))
+
+echo Configuration files checked.
+
+echo Checking dependencies...
+if exist "..\node_modules" (
+    echo Using node_modules from parent directory...
+    cd ..
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent crypto-js
+    cd %~dp0
+) else (
+    echo Installing dependencies in current directory...
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent crypto-js
+)
+echo Dependencies installation completed!
+
+echo Starting the bot...
+node meomundep
+
+pause
+exit
